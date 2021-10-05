51b5daa29693d8de4d5fee5e8bf2951046b6f1e9
paul66853 <paul66853@gmail.com>
2021-10-05 15:38:25 +0800

    一階段 還沒用FIREBASE

---
+++ app/src/main/java/kotlin_message/com/LoginActivity.kt
@@ -0,0 +1,14 @@
+package kotlin_message.com
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login) //頁面轉換
+
+    }
+}
--- app/src/main/java/kotlin_message/com/MainActivity.kt
+++ app/src/main/java/kotlin_message/com/MainActivity.kt
@@ -1,11 +1,34 @@
 package kotlin_message.com
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity","Email is:"+email)
+            Log.d("MainActivity","Password:$password")
+
+            //Firebase使用者使用Email和Password創建帳戶
+
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d("MainActivity","Try to show login activity")
+
+            //以某種方式啟動登入活動
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
