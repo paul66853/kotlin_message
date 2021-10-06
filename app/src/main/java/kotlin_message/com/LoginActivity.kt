18031e492ca8f4cdd52446908bef7ce8ce40e69c
paul66853 <paul66853@gmail.com>
2021-10-07 00:55:03 +0800

    做完註冊帳號登入資料庫

--- app/src/main/java/kotlin_message/com/LoginActivity.kt
+++ app/src/main/java/kotlin_message/com/LoginActivity.kt
@@ -1,7 +1,10 @@
 package kotlin_message.com
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
 
@@ -10,5 +13,35 @@ class LoginActivity: AppCompatActivity(){
 
         setContentView(R.layout.activity_login) //頁面轉換
 
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login","Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    //不確定!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    //若成功
+                    val result = it.result  // Kotlin says result is of type "AuthResult?"
+                    if (result != null) {
+                        val user = result.user   // Kotlin says user is of type "FirebaseUser?"
+                        if (user != null) {
+                            Log.d("Main", "Successfully sign in user with uid: ${user.uid}")
+                        }
+                    }
+                }
+                    //不確定TOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                .addOnFailureListener {
+                    if (it.message != null) {
+                        Log.d("Main","Fail to sign in user: ${it.message}")
+                    }
+                }
+        }
+
+        back_to_register_textview.setOnClickListener {
+            finish() //回原本的畫面
+        }
     }
 }
--- app/src/main/java/kotlin_message/com/MainActivity.kt
+++ app/src/main/java/kotlin_message/com/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("MainActivity","Email is:"+email)
-            Log.d("MainActivity","Password:$password")
-
-            //Firebase使用者使用Email和Password創建帳戶
-
+            performRegister() //將FUN寫到下面
         }
 
         already_have_account_text_view.setOnClickListener {
@@ -31,4 +28,40 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    //FUN建立
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        //空值時不錯誤
+        if(email.isEmpty() || password.isEmpty()) {
+            //顯示空值時的提示
+            Toast.makeText(this,"請輸入帳號和密碼",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity","Email is:"+email)
+        Log.d("MainActivity","Password:$password")
+
+        //Firebase使用者使用Email和Password創建帳戶
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                //若成功
+                val result = it.result  // Kotlin says result is of type "AuthResult?"
+                if (result != null) {
+                    val user = result.user   // Kotlin says user is of type "FirebaseUser?"
+                    if (user != null) {
+                        Log.d("Main", "Successfully created user with uid: ${user.uid}")
+                    }
+                }
+            }
+            .addOnFailureListener {
+                if (it.message != null) {
+                    Log.d("Main","Fail to create user: ${it.message}")
+                }
+            }
+    }
+
 }
