178e64412d82cdf9c551b2be265ab5ad27769364
paul66853 <paul66853@gmail.com>
2021-10-14 00:31:23 +0800

    在建置STORAGE和資料庫

--- app/src/main/java/kotlin_message/com/RegisterActivity.kt
+++ app/src/main/java/kotlin_message/com/RegisterActivity.kt
@@ -1,8 +1,12 @@
 package kotlin_message.com
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.FirebaseException
@@ -10,7 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,13 +24,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         already_have_account_text_view.setOnClickListener {
-            Log.d("MainActivity","Try to show login activity")
+            Log.d("RegisterActivity","Try to show login activity")
 
             //以某種方式啟動登入活動
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
+        selectphoto_button_register.setOnClickListener {
+            Log.d("RegisterActivity","Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK) //這要GOOGLE!!!!!!!!
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+        }
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data !=null){
+            //繼續並確認選擇的照片是甚麼
+            Log.d("RegisterActivity","Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val  bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+
+            //UI顯示照片在按鈕中
+            val  bitmapDrawable = BitmapDrawable(bitmap)
+            selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
     }
     //FUN建立
     private fun performRegister(){
@@ -40,8 +70,8 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        Log.d("MainActivity","Email is:"+email)
-        Log.d("MainActivity","Password:$password")
+        Log.d("RegisterActivity","Email is:"+email)
+        Log.d("RegisterActivity","Password:$password")
 
         //Firebase使用者使用Email和Password創建帳戶
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
@@ -54,6 +84,9 @@ class MainActivity : AppCompatActivity() {
                     val user = result.user   // Kotlin says user is of type "FirebaseUser?"
                     if (user != null) {
                         Log.d("Main", "Successfully created user with uid: ${user.uid}")
+
+                        //將圖片上載到FIREBASE
+                        uploadImageToFirebaseStorage()
                     }
                 }
             }
@@ -63,6 +96,9 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this,"註冊失敗 : ${it.message}",Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+    private fun uploadImageToFirebaseStorage(){
+
     }
 
 }
