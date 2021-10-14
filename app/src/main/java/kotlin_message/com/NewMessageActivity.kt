539c593d7b8ffd8c88d81a2988f7f574985bdcb3
paul66853 <paul66853@gmail.com>
2021-10-15 00:45:51 +0800

    畢卡索和Groupie問題解決

--- app/src/main/java/kotlin_message/com/NewMessageActivity.kt
+++ app/src/main/java/kotlin_message/com/NewMessageActivity.kt
@@ -3,7 +3,11 @@ package kotlin_message.com
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
+
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +17,17 @@ class NewMessageActivity : AppCompatActivity() {
         //更改上方BAR的字
         supportActionBar?.title = "Select User"
 
-        recycleview_newmessage.adapter = ??? //這是甚麼?
+        val adapter = GroupAdapter<GroupieViewHolder>()
+
+        recycleview_newmessage.adapter = adapter //這是甚麼?
 
     }
 }
+
+//這是令人乏味的
+
+//class CustomAdapter: RecyclerView.Adapter<ViewHolder>{
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//}
